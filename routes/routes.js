b5a15861ad426209251b1b5e901ddca11a46c9b8
madolfohuerta <m.adolfohuerta@gmail.com>
2023-03-20 10:00:41 -0600

    endpoints listos

--- routes/routes.js
+++ routes/routes.js
@@ -16,4 +16,31 @@ router.get('/datos', async (req, res) => {
 })
   
 
-  module.exports = router;
+// Definir la ruta para obtener un dato en particular
+router.get('/datos/:id', async (req, res) => {
+    const user = await Productos.findById(req.params.id)
+    res.status(200).send({
+      status: 'Success',
+      data: user,
+    })
+})
+
+//obtener datos por busqueda de nombre de producto
+router.post('/datos/busqueda', async (req, res) => {
+    const user = await Productos.find({busqueda: req.body.busqueda})
+    res.status(200).send({
+      status: 'Success',
+      data: user,
+    })
+})
+
+//obtener datos por busqueda de nombre de producto , fecha y tienda
+router.post('/datos/busqueda/datos-busqueda', async (req, res) => {
+  const user = await Productos.find({busqueda: req.body.busqueda, fecha: req.body.fecha, tienda: req.body.tienda})
+  res.status(200).send({
+    status: 'Success',
+    data: user,
+  })
+})
+
+module.exports = router;
