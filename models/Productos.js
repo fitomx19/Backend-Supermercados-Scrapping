2b53ff5f594e2b94986d1d80d7715f6bb3bea424
madolfohuerta <m.adolfohuerta@gmail.com>
2023-04-09 12:42:36 -0600

    Talent land ready ->

--- models/Productos.js
+++ models/Productos.js
@@ -8,7 +8,12 @@ const ProductosSchema = new Schema({
   busqueda: String,
   nombre: String,
   url: String,
-  fecha: String
+  fecha: String,
+  tienda: String,
+  fecha: String,
+  gramaje: Number,
+  precioPorGramo: Number,
+  palabrasClaveEncontradas: [String]
 });
 
 module.exports = mongoose.model("Productos", ProductosSchema);
--- routes/routes.js
+++ routes/routes.js
@@ -8,14 +8,62 @@ var mongoose = require('mongoose');
 
 // Definir la ruta para obtener todos los datos
 router.get('/datos', async (req, res) => {
-    const allUsers = await Productos.find()
+    const datos = await Productos.find()
+    //ordenar los datos por fecha de manera descendente
+    datos.sort(function(a, b) {
+      return new Date(b.fecha) - new Date(a.fecha);
+    });
+
     res.status(200).send({
       status: 'Success',
-      data: allUsers,
+      data: datos,
     })
 })
-  
 
+router.get('/keywords', async (req, res) => {
+  try {
+    const result = await Productos.aggregate([
+      { $match: { palabrasClaveEncontradas: { $exists: true } } },
+      { $unwind: "$palabrasClaveEncontradas" },
+      {
+        $group: {
+          _id: "$palabrasClaveEncontradas",
+          count: { $sum: 1 },
+        },
+      },
+    ]);
+    const lista = result.map((item) => item._id);
+    res.status(200).send({
+      status: "Success",
+      lista: lista,
+      data: result,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({
+      status: "Error",
+      message: "Error al obtener los datos",
+    });
+  }
+});
+
+router.get('/supermercados/:nombre', async (req, res) => {
+  let supermercado = req.params.nombre;
+
+  const tiendas = await Productos.find({tienda: supermercado})
+
+  //Agrupar los productos por nombre
+/*   let productosAgrupados = tiendas.reduce((r, a) => {
+    r[a.nombre] = [...(r[a.nombre] || []), a];
+    return r;
+  }, {}); */
+
+  res.status(200).send({
+    status: 'Success',
+    data: tiendas,
+  })
+})
+  
 // Definir la ruta para obtener un dato en particular
 router.get('/datos/:id', async (req, res) => {
     const user = await Productos.findById(req.params.id)
@@ -43,4 +91,46 @@ router.post('/datos/busqueda/datos-busqueda', async (req, res) => {
   })
 })
 
+
+//comparar precios de productos dados las keywords y la ultima fecha de busqueda
+router.post('/datos/busqueda/ultima-busqueda', async (req, res) => {
+
+  //obtener el json de la busqueda
+  const temp = req.body
+  console.log(temp)
+   //extraer el id del producto
+  const _id = temp._id
+  //buscar el producto por id
+  const producto = await Productos.findById(_id)
+  
+  //mapear el array de palabras clave encontradas
+  const resultados = []
+
+  await Promise.all(producto.palabrasClaveEncontradas.map(async (palabraClave) => {
+    const resultado = await Productos.find({palabrasClaveEncontradas: palabraClave})
+    resultados.push(resultado)
+  })) 
+
+  
+  //agrupar los productos por nombre y por tienda
+  const productosPorNombre = resultados.flat().reduce((acc, producto) => {
+    if (acc[producto.nombre]) {
+      acc[producto.nombre].push(producto);
+    } else {
+      acc[producto.nombre] = [producto];
+    }
+    return acc;
+  }, {});
+
+  console.log(productosPorNombre)
+  //imprimir productosAgrupadosPorTienda
+  //console.log(productosPorNombre)
+  res.status(200).send({
+    status: 'Success',
+    data: productosPorNombre
+  })    
+})
+
+
 module.exports = router;
+
